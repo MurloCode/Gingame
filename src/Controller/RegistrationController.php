8c3e77bd6140a086dbb12ef8a72514c6793843ab
MarryMe78 <lecullier.robin@gmail.com>
2021-07-19 10:22:40 +0200

    dev recreated after merge problem

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -23,7 +23,7 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // encode the plain password
+            // encode the plain password.
             $user->setPassword(
                 $passwordHasher->hashPassword(
                     $user,
