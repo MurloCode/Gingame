861f95d177ebee178e740700b8dc547af6bb7e75
Corentin Marillier <marillier.io@gmail.com>
2021-08-03 10:51:55 +0200

    new upload image in edit user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
@@ -28,6 +30,24 @@ class UserType extends AbstractType
             ])
             ->add('Created_At')
             ->add('friends')
+            ->add('image', FileType::class, [
+                'label' => 'Charger une nouvelle image',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                    ])
+                ],
+            ])
             ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
                 // Avant construction du formulaire, on va d'abord vérifier dans
                 // quel contexte on se trouve :
