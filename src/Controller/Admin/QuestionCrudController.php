ae8f29c7b09dbf927f1237993225f3a70c4bcf44
Fanou <stephane@roxsys.net>
2021-08-05 12:37:22 +0200

    details on EasyAdmin

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use App\Form\PropositionType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
@@ -12,20 +16,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  */
 class QuestionCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Question::class;
-    }
+	public static function getEntityFqcn(): string
+	{
+		return Question::class;
+	}
 
-    
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            'id',
-            'question',
-            AssociationField::new('propositions'),
-            AssociationField::new('quizzs'),
-        ];
-    }
-    
+	
+	public function configureFields(string $pageName): iterable
+	{
+		return [
+			IdField::new('id')
+				->onlyOnIndex(),
+
+			'question',
+
+			// AssociationField::new('propositions'),
+			CollectionField::new('propositions')
+				->setLabel('Propositions')
+				->setCssClass('quizzCSS')
+				->setFormtypeOption("mapped",false)
+				->setEntryType(PropositionType::class)
+				// ->setEntryIsComplex(true)
+				// ->allowAdd()
+				// ->allowDelete()
+				->hideOnIndex(),
+				
+			AssociationField::new('quizzs'),
+		];
+	}
+	
 }
