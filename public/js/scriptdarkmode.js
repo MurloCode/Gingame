c58ff0dd6a616664e99c6da8817e01e606e9fd8d
Daixois <thomas.dimas.goux@gmail.com>
2021-08-07 19:13:47 +0200

    night mode added

--- public/js/scriptdarkmode.js
+++ public/js/scriptdarkmode.js
@@ -1,4 +1,9 @@
-const btnToggle = document.querySelector('.btn-toggle');
+window.addEventListener("load",function(){
+  
+const btnToggle = document.getElementById('btnToggle');
+const img = document.getElementById('imgToggle')
+
+
 //  Listen the Event Click to Switch from Light to Dark and from Dark to Light
 btnToggle.addEventListener('click', ()=> {
 
@@ -8,13 +13,19 @@ btnToggle.addEventListener('click', ()=> {
 
         body.classList.add('light')
         body.classList.remove('dark')
-        btnToggle.innerHTML = "N"
-
+        imgToggle.classList.add('nuit')
+        imgToggle.classList.remove('jour')
+        document.cookie = "darkMode=on"
 
     } else if(body.classList.contains('light')){
 
         body.classList.add('dark')
         body.classList.remove('light')
-        btnToggle.innerHTML = "J"
+        imgToggle.classList.add('jour')
+        imgToggle.classList.remove('nuit')
+        document.cookie = "darkMode=off"
     }
+
+}) 
+    
 })
